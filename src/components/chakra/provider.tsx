83f91384ea97661c933888076204bf9ab50e1f9b
khwong-c <kin.hin.wong.c@gmail.com>
2025-08-10 17:41:58 +0100

    fix: Provider Children Argument

--- src/components/chakra/provider.tsx
+++ src/components/chakra/provider.tsx
@@ -1,12 +1,9 @@
 "use client"
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import {ChakraProvider, defaultSystem} from "@chakra-ui/react"
 import * as React from "react"
 
-export function Provider(children: React.ReactNode) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      {children}
-    </ChakraProvider>
-  )
-}
+export const Provider = (
+    props: { children: React.ReactNode }
+) =>
+    <ChakraProvider value={defaultSystem} {...props}/>;
