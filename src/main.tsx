6fbc3a9a0e8fe3d64e8e8f216b119736ec9fa3b6
khwong-c <kin.hin.wong.c@gmail.com>
2025-08-10 18:05:04 +0100

    fix: Fix to Dark mode for development

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {Provider} from './components/chakra/provider'
+import {DarkMode} from "./components/chakra/color-mode.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider>
-            <App/>
+            <DarkMode>
+                <App/>
+            </DarkMode>
         </Provider>
     </StrictMode>,
 )
