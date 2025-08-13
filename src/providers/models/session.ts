bc0c78c343237d4cf3e64d28dc2bc08c1964b7af
khwong-c <kin.hin.wong.c@gmail.com>
2025-08-13 21:28:04 +0100

    feat: Revise Sessions Model

--- src/providers/models/session.ts
+++ src/providers/models/session.ts
@@ -1,34 +1,46 @@
-import {useContext, createContext} from "react";
-import * as React from "react";
+export const SessionType = {
+    Work: "WORK",
+    Rest: "REST",
+}
+export type SessionTypeEnum = typeof SessionType[keyof typeof SessionType];
 
-export const SessionTypeWORK = "WORK", SessionTypeBREAK = "BREAK";
+export const DispatchPresetSession = {
+    Preset503020: "Preset-50-30-20",
+}
+export type DispatchPresetSessionEnum = typeof DispatchPresetSession[keyof typeof DispatchPresetSession];
+
+export interface Session {
+    type: SessionTypeEnum;
+    length: number; // In seconds
+}
+
+export const dispatchPresetSession = (preset: DispatchPresetSessionEnum): Session[] => {
+    const minutes = 60;
+    switch (preset) {
+        case DispatchPresetSession.Preset503020:
+            return [
+                {type: SessionType.Work, length: 50 * minutes},
+                {type: SessionType.Rest, length: 10 * minutes},
+                {type: SessionType.Work, length: 30 * minutes},
+                {type: SessionType.Rest, length: 10 * minutes},
+                {type: SessionType.Work, length: 20 * minutes},
+                {type: SessionType.Rest, length: 10 * minutes},
+            ]
+        default:
+            return [];
+    }
+}
 
 export interface SessionEvents {
     onSessionStart: (curSession: number) => void;
     onSessionEnd: (curSession: number) => void;
-    onSessionChange: (curSession: number, sessionType: string) => void;
+    onSessionChange: (curSession: number, sessionType: SessionTypeEnum) => void;
     onSessionAllSessionDone: () => void;
 }
 
 export interface SessionState {
-    totalSessions: number;
+    presetSession: DispatchPresetSessionEnum | null;
+    sessionProgram: Session[];
     currentSession: number;
-    sessionState: string;
-    workSessionLength: number; // in seconds
-    breakSessionLength: number; // in seconds
     eventCallbacks: SessionEvents;
-}
-
-export interface SessionReduceCommand {
-    type: string
-}
-
-export const SessionContext = createContext<{
-    state: SessionState,
-    dispatch: React.Dispatch<SessionReduceCommand>
-} | null>(null);
-
-
-export function useSessionState() {
-    return useContext(SessionContext);
 }
