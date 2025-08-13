5e9cf04dc16412dc0bea899f501630556e4d0f69
khwong-c <kin.hin.wong.c@gmail.com>
2025-08-13 21:29:30 +0100

    feat: Prest program selector

--- src/components/session-control/panel.tsx
+++ src/components/session-control/panel.tsx
@@ -1,46 +1,77 @@
-import {Button, Drawer, Field, Fieldset, For, Input, NativeSelect, Stack} from "@chakra-ui/react";
+import {
+    Button, Container,
+    createListCollection,
+    Drawer,
+    Fieldset,
+    Stack,
+} from "@chakra-ui/react";
+import * as React from "react";
+
+import {SingleSelector} from "../chakra/select";
+import {useSessionState} from "../../providers/session-hooks.ts";
+import {
+    SessionDispatchCommand,
+} from "../../providers/reducers/session.ts"
+import {
+    DispatchPresetSession,
+    type DispatchPresetSessionEnum,
+} from "../../providers/models/session.ts";
+
+const presetChoices = createListCollection({
+    items: [
+        {label: "50-30-20 Mins Program", value: DispatchPresetSession.Preset503020},
+    ],
+})
+
+
+const PresetSessionSelect = (
+    props: {
+        value: string,
+        onChange: (value: DispatchPresetSessionEnum) => void,
+    }
+) => {
+    const {value, onChange} = props;
+    return (
+        <SingleSelector
+            collection={presetChoices}
+            label={"Preset Program"}
+            helperText={"Select a preset program"}
+            value={value}
+            onChange={onChange}
+        />
+    )
+}
+
 
 const SessionPanel = () => {
+    const {state, dispatch} = useSessionState();
     return (
-        <Fieldset.Root flex={4} bg="gray.700" alignItems={"center"} p={4}>
-            <Stack>
-                <Fieldset.Legend>Contact details</Fieldset.Legend>
-                <Fieldset.HelperText>
-                    Please provide your contact details below.
-                </Fieldset.HelperText>
-            </Stack>
-            <Fieldset.Content>
-                <Field.Root>
-                    <Field.Label>Name</Field.Label>
-                    <Input name="name"/>
-                </Field.Root>
-
-                <Field.Root>
-                    <Field.Label>Email address</Field.Label>
-                    <Input name="email" type="email"/>
-                </Field.Root>
-
-                <Field.Root>
-                    <Field.Label>Country</Field.Label>
-                    <NativeSelect.Root>
-                        <NativeSelect.Field name="country">
-                            <For each={["United Kingdom", "Canada", "United States"]}>
-                                {(item) => (
-                                    <option key={item} value={item}>
-                                        {item}
-                                    </option>
-                                )}
-                            </For>
-                        </NativeSelect.Field>
-                        <NativeSelect.Indicator/>
-                    </NativeSelect.Root>
-                </Field.Root>
-            </Fieldset.Content>
-            <Fieldset.Content/>
+        <Container flex={4}>
+            <Fieldset.Root flex={4} bg="gray.700" alignItems={"center"} p={4}>
+                <Stack>
+                    <Fieldset.Legend>Session Setting</Fieldset.Legend>
+                    <Fieldset.HelperText>
+                        TODO: FILL IN THE BLANKS
+                    </Fieldset.HelperText>
+                </Stack>
+                <Fieldset.Content>
+                    <PresetSessionSelect
+                        value={state.presetSession ?? ""}
+                        onChange={(value) => {
+                            dispatch({
+                                type: SessionDispatchCommand.ChangeSessionPreset,
+                                payload: {preset: value},
+                            })
+                        }}
+                    />
+                </Fieldset.Content>
+            </Fieldset.Root>
             <Drawer.Trigger>
                 <Button>Start Session</Button>
             </Drawer.Trigger>
-        </Fieldset.Root>
-    )
+        </Container>
+
+    );
 }
+
 export default SessionPanel;
